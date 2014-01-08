0b46afbc5426e38aa35489e2ba5a8d2e4dd7eb17
Lucas Weiblen <lucasweiblen@gmail.com>
2014-01-08 00:29:50 -0200

    Almost complete sign up feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    redirect_to root_url
+    login = Login.new(login_params)
+    session[:reader_id] = login.authenticate
+    redirect_to root_url, notice: 'Youre logged in'
+  end
+
+  private
+
+  def login_params
+    params.require(:login).permit(:email, :password)
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -21,4 +21,43 @@ describe SessionsController do
       expect(assigns[:login]).to eq login
     end
   end
+
+  describe 'post create' do
+    let(:params) { { email: 'email@email.com', password: 'pass' } }
+    let!(:login) { stub_model(Login) }
+
+    before do
+      allow(Login).to receive(:new).and_return(login)
+    end
+
+    context 'when data is valid' do
+      before do
+        allow(login).to receive(:valid?).and_return(true)
+      end
+
+      it 'sends authenticate message to Login model' do
+        expect(login).to receive(:authenticate)
+        post :create, login: params
+      end
+
+      context 'when authenticate method returns true' do
+        before do
+          allow(login).to receive(:authenticate).and_return(true)
+          post :create, login: params
+        end
+        
+        it 'redirects to root url' do
+          expect(response).to redirect_to root_url
+        end
+
+        it 'assigns success message' do
+          expect(flash[:notice]).not_to be_nil
+        end
+
+        it 'authenticates reader' do
+          expect(session[:reader_id]).not_to be_nil
+        end
+      end
+    end
+  end
 end
